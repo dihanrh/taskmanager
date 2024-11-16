94c1c18af3ef1741b8555b3bc957acbd3b981766
dihanrh <code.dihan@gmail.com>
2024-11-16 11:16:29 +0600

    layout : navbar

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { toggleDarkMode } from '../redux/features/theme/themeSlice';
+import Modal from '../components/common/Modal';
+import TaskForm from '../components/task/TaskForm';
 
 const Navbar: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const dispatch = useDispatch();
 
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
+
   return (
-    <div className="flex items-center justify-between p-4 bg-white dark:bg-gray-800 shadow-md">
-      <div className="flex items-center">
-        <button
-          className="px-4 py-2 bg-blue-500 text-white rounded"
-          onClick={() => console.log('Add Task clicked')}
-        >
-          Add Task
-        </button>
+    <>
+      <div className="flex items-center justify-between p-4 bg-white dark:bg-gray-800 shadow-md">
+        <div className="flex items-center">
+          <button
+            className="px-4 py-2 bg-blue-500 text-white rounded"
+            onClick={handleOpenModal}
+          >
+            Add Task
+          </button>
+        </div>
+        <div className="flex items-center space-x-4">
+          <button
+            className="px-4 py-2 text-gray-800 dark:text-white"
+            onClick={() => dispatch(toggleDarkMode())}
+          >
+            Toggle Dark Mode
+          </button>
+          <span className="text-gray-800 dark:text-white">Notification</span>
+        </div>
       </div>
-      <div className="flex items-center space-x-4">
-        <button onClick={() => dispatch(toggleDarkMode())}>
-          Toggle Dark Mode
-        </button>
-        <span>Notification</span>
-      </div>
-    </div>
+
+  
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+        <TaskForm onClose={handleCloseModal} />
+      </Modal>
+    </>
   );
 };
 
